d9ad1f6cd568b207a8941b19bb515ea432fd4398
anthony <acarrillorj@gmail.com>
2016-08-22 11:52:18 -0500

    ordenamiento de fechas

--- gui/view/VistaVerPaciente.php
+++ gui/view/VistaVerPaciente.php
@@ -159,6 +159,27 @@
 	}
 
 	$menoscabo = $boMenoscabo->GetEntidadxTrabajador($idTrabajador);
+
+	function burbujaFechas($array, $n) {
+		    for ($i = 1; $i < $n; $i++) {
+		      for ($j = 0; $j < $n - $i; $j++) {
+		        if ($array[$j] > $array[$j + 1]) {
+		          $k = $array[$j + 1]; 
+		          $array[$j + 1] = $array[$j]; 
+		          $array[$j] = $k;
+		        }
+		      }
+		    }
+		 
+		return $array;
+	}
+
+	for($j=0; $j < count($audioTonalOd); $j++){
+		$array2[$j] = strtotime($otoscopia[$j]->fechaAudiometria);
+	}
+	
+	$burbuja = burbujaFechas($array2, count($array2));
+
 ?>
 
 <!DOCTYPE html>
@@ -278,6 +299,8 @@
 		  		}
 	  		}
   		}
+
+  		
 
   	</script>
 
@@ -514,38 +537,51 @@
 									</thead>
 									<tbody>
 										<?php
-										for ($i=0; $i < count($audioTonalOd); $i++) {
-										?>
-										<tr>
-											<td height="5px;"><center><?php echo $i+1; ?></center></td>
-											<input type="hidden" name="<?php echo 'idAudioTonalOd_'.$i;?>" id="<?php echo 'idAudioTonalOd_'.$i;?>" value="<?php echo $audioTonalOd[$i]->idAudioTonalOd; ?>">
-											<input type="hidden" name="<?php echo 'idAudioTonalOi_'.$i;?>" id="<?php echo 'idAudioTonalOi_'.$i;?>" value="<?php echo $audioTonalOi[$i]->idAudioTonalOi; ?>">
-											<td><input class="form-control" type="radio" onClick="EventoRadioLb(<?php echo $i;?>)" name="lbRadio_od" id="<?php echo 'lbRadio_od_'.$i;?>" value="" <?php if($ultOd==$audioTonalOd[$i]->idAudioTonalOd){echo 'checked';}?>></td>
-											<td><input class="form-control" type="radio" onClick="EventoRadioCt(<?php echo $i;?>)" name="ctRadio_od" id="<?php echo 'ctRadio_od_'.$i;?>" value=""></td>
-											<td><input class="form-control" type="checkbox" name="retestCheck" id="<?php echo 'retestCheck_'.$i;?>" value="" <?php if($audioTonalOd[$i]->retest==1){echo 'checked';}?>></td>
-											<td><input class="form-control" type="checkbox" name="failCheck" id="<?php echo 'failCheck_'.$i;?>" value="" <?php if($audioTonalOd[$i]->fail==1){echo 'checked';}?>></td>
-											<td><span><center><?php echo substr($otoscopia[$i]->fechaAudiometria,8,2).'-'.substr($otoscopia[$i]->fechaAudiometria,5,2).'-'.substr($otoscopia[$i]->fechaAudiometria,0,4); ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOd[$i]->od_500; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOd[$i]->od_1000; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOd[$i]->od_2000; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOd[$i]->od_3000; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOd[$i]->od_4000; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOd[$i]->od_6000; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOd[$i]->od_8000; ?></center></span></td>
-											<td><center><?php if($audioTonalOd[$i]->od_sts>=10){echo '<span style="color:#FF0000;">'.$audioTonalOd[$i]->od_sts.'</span>';}else{echo '<span>'.$audioTonalOd[$i]->od_sts.'</span>';} ?></center></td>
-											<td><span><center><?php echo $audioTonalOd[$i]->od_khz; ?></center></span></td>
 
-											<td><span><center><?php echo $audioTonalOi[$i]->oi_500; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOi[$i]->oi_1000; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOi[$i]->oi_2000; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOi[$i]->oi_3000; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOi[$i]->oi_4000; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOi[$i]->oi_6000; ?></center></span></td>
-											<td><span><center><?php echo $audioTonalOi[$i]->oi_8000; ?></center></span></td>
-											<td><center><?php if($audioTonalOi[$i]->oi_sts>=10){echo '<span style="color:#FF0000;">'.$audioTonalOi[$i]->oi_sts.'</span>';}else{echo '<span>'.$audioTonalOi[$i]->oi_sts.'</span>';} ?></center></td>
-											<td><span><center><?php echo $audioTonalOi[$i]->oi_khz; ?></center></span></td>
-										</tr>
+										
+										for ($i = 0; $i < count($burbuja); $i++) {
+										     echo $burbuja[$i] ." ";
+										
+										}
+										// for ($i=0; $i < count($burbuja); $i++) {
+										// 	if(strtotime($burbuja[$i] == $otoscopia[$i]->fechaAudiometria)){
+										for ($j = 0; $j < count($burbuja); $j++) {
+		                                    for ($i = 0; $i < count($burbuja); $i++) {
+		        								if ( $burbuja[$j] == strtotime($otoscopia[$i]->fechaAudiometria)) {
+
+										?>
+													<tr>
+														<td height="5px;"><center><?php echo $i+1; ?></center></td>
+														<input type="hidden" name="<?php echo 'idAudioTonalOd_'.$i;?>" id="<?php echo 'idAudioTonalOd_'.$i;?>" value="<?php echo $audioTonalOd[$i]->idAudioTonalOd; ?>">
+														<input type="hidden" name="<?php echo 'idAudioTonalOi_'.$i;?>" id="<?php echo 'idAudioTonalOi_'.$i;?>" value="<?php echo $audioTonalOi[$i]->idAudioTonalOi; ?>">
+														<td><input class="form-control" type="radio" onClick="EventoRadioLb(<?php echo $i;?>)" name="lbRadio_od" id="<?php echo 'lbRadio_od_'.$i;?>" value="" <?php if($ultOd==$audioTonalOd[$i]->idAudioTonalOd){echo 'checked';}?>></td>
+														<td><input class="form-control" type="radio" onClick="EventoRadioCt(<?php echo $i;?>)" name="ctRadio_od" id="<?php echo 'ctRadio_od_'.$i;?>" value=""></td>
+														<td><input class="form-control" type="checkbox" name="retestCheck" id="<?php echo 'retestCheck_'.$i;?>" value="" <?php if($audioTonalOd[$i]->retest==1){echo 'checked';}?>></td>
+														<td><input class="form-control" type="checkbox" name="failCheck" id="<?php echo 'failCheck_'.$i;?>" value="" <?php if($audioTonalOd[$i]->fail==1){echo 'checked';}?>></td>
+														<td><span><center><?php echo substr($otoscopia[$i]->fechaAudiometria,8,2).'-'.substr($otoscopia[$i]->fechaAudiometria,5,2).'-'.substr($otoscopia[$i]->fechaAudiometria,0,4); ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOd[$i]->od_500; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOd[$i]->od_1000; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOd[$i]->od_2000; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOd[$i]->od_3000; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOd[$i]->od_4000; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOd[$i]->od_6000; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOd[$i]->od_8000; ?></center></span></td>
+														<td><center><?php if($audioTonalOd[$i]->od_sts>=10){echo '<span style="color:#FF0000;">'.$audioTonalOd[$i]->od_sts.'</span>';}else{echo '<span>'.$audioTonalOd[$i]->od_sts.'</span>';} ?></center></td>
+														<td><span><center><?php echo $audioTonalOd[$i]->od_khz; ?></center></span></td>
+
+														<td><span><center><?php echo $audioTonalOi[$i]->oi_500; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOi[$i]->oi_1000; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOi[$i]->oi_2000; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOi[$i]->oi_3000; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOi[$i]->oi_4000; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOi[$i]->oi_6000; ?></center></span></td>
+														<td><span><center><?php echo $audioTonalOi[$i]->oi_8000; ?></center></span></td>
+														<td><center><?php if($audioTonalOi[$i]->oi_sts>=10){echo '<span style="color:#FF0000;">'.$audioTonalOi[$i]->oi_sts.'</span>';}else{echo '<span>'.$audioTonalOi[$i]->oi_sts.'</span>';} ?></center></td>
+														<td><span><center><?php echo $audioTonalOi[$i]->oi_khz; ?></center></span></td>
+													</tr>
 										<?php
+												}
+											}
 										}
 										?>
 									</tbody>
